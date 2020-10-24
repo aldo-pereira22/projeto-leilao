360ef55e88990babb9ab128d4d9a0627a71b39cb
aldo-pereira22 <aldopereira.gyn@gmail.com>
2020-10-24 20:12:25 -0300

    Editar/Excluir leiloes- refatorado
    Editar e exluir editados.
    Excluir está funcionando.
    Editar tendo alguns problemas, ao invés de editar, está salvando duas vezes.

--- src/controlador/LeilaoServlet.java
+++ src/controlador/LeilaoServlet.java
@@ -14,122 +14,85 @@ import com.google.gson.Gson;
 import dao.LeilaoDao;
 import entidade.Leilao;
 
-
 @WebServlet(urlPatterns = "/leilao")
-public class LeilaoServlet extends HttpServlet{
-		
-	
-	
+public class LeilaoServlet extends HttpServlet {
+
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		
-	
-		LeilaoDao ldao = new LeilaoDao();		
-		Leilao leilao = new Leilao();	
-		
-		String descricao = req.getParameter("input-descricao");		
+
+		LeilaoDao ldao = new LeilaoDao();
+		Leilao leilao = new Leilao();
+
+		String descricao = req.getParameter("input-descricao");
 		String valorInicial = req.getParameter("input-valor-inicial");
 		String valorArremate = req.getParameter("input-valor-arremate");
 		String dataCriacao = req.getParameter("input-data-criacao");
 		String situacao = req.getParameter("situacao");
-		
-		System.out.println("Descrição - "+ descricao);
-		System.out.println("VALOR INICIAL - " + valorInicial );
-		System.out.println("VALOR ARREMATE - "+ valorArremate);
-		System.out.println("DATA CRIACAO - "+ dataCriacao);
-		System.out.println("SITUACAO -  "+ situacao);
-		
-	
-		
+
+		System.out.println("Descrição - " + descricao);
+		System.out.println("VALOR INICIAL - " + valorInicial);
+		System.out.println("VALOR ARREMATE - " + valorArremate);
+		System.out.println("DATA CRIACAO - " + dataCriacao);
+		System.out.println("SITUACAO -  " + situacao);
+
 		leilao.setDescricao(descricao);
 		leilao.setDataCriacao(dataCriacao);
 		leilao.setValorArremate(Double.parseDouble(valorArremate));
-		leilao.setValorInicial( Double.parseDouble(valorInicial));
+		leilao.setValorInicial(Double.parseDouble(valorInicial));
 		leilao.setSituacao(situacao);
-		ldao.salvar(leilao);		
+		ldao.salvar(leilao);
 		resp.sendRedirect("leiloes.html");
-		
+
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		System.out.println(req.getParameterNames().toString());
+
 		LeilaoDao ldao = new LeilaoDao();
 		Gson gson = new Gson();
-		
+
 		String descricao = req.getParameter("descricao");
-		if(descricao ==null) {
+		if (descricao == null) {
 			resp.setContentType("aplication/json");
 			resp.setCharacterEncoding("UTF-8");
 			resp.getWriter().write(gson.toJson(ldao.lista()));
-		}else {
-			
+		} else {
+
 			String operacao = req.getParameter("operacao");
-			if(operacao != null && operacao.equals("excluir")) {
-				
+			if (operacao != null && operacao.equals("excluir")) {
+				System.out.println("LINHA 73");
 				Leilao leilao = new Leilao();
 				List<Leilao> listaLeilao = ldao.lista();
-				
-				for(Leilao l : listaLeilao) {
-					if(l.getDescricao().equals(descricao)) {
-						System.out.println("DESCRICÂO: -------"+l.getDescricao());
+
+				for (Leilao l : listaLeilao) {
+					if (l.getDescricao().equals(descricao)) {
+						System.out.println("DESCRICÂO: -------" + l.getDescricao());
 						leilao = l;
-						return;
 					}
 				}
-				System.out.println("Leilão Descricção:"+ leilao.getDescricao());
-				System.out.println("Leilão Valor Arremate:"+ leilao.getValorArremate());
-				System.out.println("Leilão Descricção:"+ leilao.getDescricao());
 				ldao.deleta(leilao);
 				resp.sendRedirect("leiloes.html");
-			}else {
-				
-				if(req.getParameter("origem") != null && req.getParameter("origem").equals("cadastro-leilao")) {
+			} else {
+
+				if (req.getParameter("origem") != null && req.getParameter("origem").equals("cadastro-leilao")) {
 					Leilao leilao = new Leilao();
 					List<Leilao> listaLeilao = ldao.lista();
-					
-					for(Leilao l : listaLeilao) {
-						if(l.getDescricao().equals(descricao)) {
+
+					for (Leilao l : listaLeilao) {
+						if (l.getDescricao().equals(descricao)) {
 							leilao = l;
-							return;
 						}
 					}
 					String jsonLeilao = gson.toJson(leilao);
 					resp.setContentType("aplication/json");
 					resp.getWriter().print(jsonLeilao.toString());
-				}else {
-					resp.sendRedirect("cadastro-leilao.html?descricao="+req.getParameter("descricao"));
+				} else {
+					resp.sendRedirect("cadastro-leilao.html?descricao=" + req.getParameter("descricao"));
+					// resp.sendRedirect("cadastro-leilao.html");
 				}
-				
+
 			}
-			
+
 		}
 
-		
 	}
-	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
